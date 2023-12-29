46c22204872b8f93c428253746a6e92201da3b9d
rjeela96 <rajashekharjeela@mirafra.com>
2023-12-29 18:02:26 +0530

    assignment done as mahesh instructed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,82 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone:true,
 })
 export class AppComponent {
-  title = 'assignment';
+  currentCard: HTMLElement | null = null;
+  displayElement: HTMLElement | null = null;
+  messages: string[] = ['Message 1', 'Message 2', 'Message 3'];
+
+  toggleCard() {
+    const buttonContainer = document.getElementById('buttonContainer');
+    const cardContainer = document.getElementById('cardContainer');
+  
+    if (this.currentCard) {
+      this.hideCard(this.currentCard);
+    } else {
+      this.createCard(buttonContainer, cardContainer);
+    }
+  }
+  
+  createCard(buttonContainer: HTMLElement | null, cardContainer: HTMLElement | null) {
+    if (!buttonContainer || !cardContainer) {
+      return;
+    }
+  
+    this.messages.forEach((messageText) => {
+      const button = document.createElement('button');
+      button.textContent = messageText;
+      button.addEventListener('click', () => this.showMessageClicked(messageText));
+      button.classList.add('bts');
+
+      buttonContainer.appendChild(button);
+    });
+  
+    this.currentCard = document.createElement('div');
+    this.currentCard.classList.add('card');
+    cardContainer.appendChild(this.currentCard);
+  }
+  
+  hideCard(card: HTMLElement | null) {
+    const buttonContainer = document.getElementById('buttonContainer');
+    const cardContainer = document.getElementById('cardContainer');
+  
+    if (buttonContainer && cardContainer && card) {
+      cardContainer.removeChild(card);
+      buttonContainer.innerHTML = '';
+      this.currentCard = null;
+      this.displayElement = null;
+    }
+  }
+  
+
+  showMessageClicked(message: string) {
+    const buttonContainer = document.getElementById('buttonContainer');
+    const cardContainer = document.getElementById('cardContainer');
+  
+    if (buttonContainer && cardContainer) {
+      buttonContainer.innerHTML = '';
+  
+
+      if (this.currentCard) {
+        cardContainer.removeChild(this.currentCard);
+      }
+
+      this.currentCard = document.createElement('div');
+      this.currentCard.classList.add('card');
+      cardContainer.appendChild(this.currentCard);
+  
+     
+      this.displayElement = document.createElement('p');
+      this.displayElement.textContent = `${message} is clicked`;
+      this.currentCard.appendChild(this.displayElement);
+    }
+  }
+  
+
+  
 }
